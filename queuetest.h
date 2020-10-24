22b318dce03cbdc54bb2263c546061916af527b9
Joshua Berkley <j708b537@ku.edu>
2020-10-24 14:42:42 -0500

    Update queuetest.h

--- queuetest.h
+++ queuetest.h
@@ -10,20 +10,20 @@
 class Queuetest : public Queue
 {
 
-private:
-    bool failed = false;
-    
-public:
-    Queuetest() {};
-    void runtests();
-    void haspassed();
-    bool test1();
-    bool test2();
-    bool test3();
-    bool test4();
-    bool test5();
-    bool test6();
-    bool test7();
+    private:
+        bool failed = false;
+        
+    public:
+        Queuetest() {};
+        void runtests();
+        void haspassed();
+        bool test1();
+        bool test2();
+        bool test3();
+        bool test4();
+        bool test5();
+        bool test6();
+        bool test7();
 
 
 };
